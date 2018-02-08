702b5f06a29558744d8fd2bef6b3ddb906609d89
doDomoGu <kaka_71936410@qq.com>
2018-02-08 10:55:18 +0800

    前台页面布局 侧边栏菜单

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -20,12 +20,28 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'css/style.css',
     ];
     public $js = [
+        'js/side-nav.js'
     ];
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
     ];
+
+    //导入当前页的功能js文件，注意加载顺序，这个应该最后调用  文件路径相对@web即可
+    public static function addJsFile($view, $jsfile) {
+        $view->registerJsFile('@web/adminAssets/'.$jsfile, ['depends' => 'app\assets\AppAdminAsset']);
+    }
+
+    //导入当前页的功能js代码，注意加载顺序，这个应该最后调用  文件路径相对@web即可
+    public static function addJs($view, $jsString) {
+        $view->registerJs($jsString, ['depends' => 'app\assets\AppAdminAsset']);
+    }
+
+    //导入当前页的样式css文件，注意加载顺序，这个应该最后调用  文件路径相对@web即可
+    public static function addCssFile($view, $cssfile) {
+        $view->registerCssFile('@web/adminAssets/'.$cssfile, ['depends' => 'app\assets\AppAdminAsset']);
+    }
 }
